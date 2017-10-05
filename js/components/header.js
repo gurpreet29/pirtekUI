8255b4c269b115169f608a3c3be8b54ae87621c7
Vinod Chaurasia <vinod.chaurasia@espire.com>
2017-10-05 17:17:10 +0530

    Header Component completed

--- js/components/header.js
+++ js/components/header.js
@@ -1,6 +1,18 @@
 /*Header component*/
+function headerNav() {
+    var topnav = $('header .top-nav-wrap .top-nav li').slice(0, 3),
+        menuDropdown = $('header .navbar-default .navbar-collapse .navbar-nav');
+    $(menuDropdown).append(topnav);
+}
 
+// GLOBAL VARIABLES
+var widownWidth = $(window).width();
+
+// READY FUNCTION
 $(document).ready(function() {
-    var topnav = $('header .top-nav-wrap .top-nav li').slice(0,3);
-    console.log(topnav);
+
+    //mobile device
+    if (widownWidth < 767) {
+        headerNav();
+    }
 });
