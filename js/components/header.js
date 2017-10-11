9221bb72d822acf2d31979a0463758e0aa7a53b0
Vinod Chaurasia <vinod.chaurasia@espire.com>
2017-10-11 18:32:58 +0530

    Quick link

--- js/components/header.js
+++ js/components/header.js
@@ -6,6 +6,44 @@ function headerNav() {
     $(menuDropdown).append(topnav);
 }
 
+// Convert svg to src
+function svgBg() {
+    $("img[src$='.svg']").each(function() {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, else we gonna set it if we can.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+}
+
+
 /*function hoverAdd() {
     if ($(window).width() > 767) {
         $(this).addClass('open');
@@ -58,6 +96,8 @@ $(document).ready(function() {
         $(this).parent().siblings().removeClass('open');
         $(this).parent().toggleClass('open');
     });
+
+    svgBg();
 
 
     // $(headerList).on('mouseenter focus', hoverAdd);
