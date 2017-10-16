45477bdc10292743ca64d56cbaa4811633385ce9
Gurpreet singh <gurpreet.singh@espire.com>
2017-10-16 14:54:40 +0530

    height for right panel to match with ".columnHeight"

---
+++ js/components/right-panel.js
@@ -0,0 +1,80 @@
+/* COMPONENT - quick link */
+window.app = window.app || {};
+
+(function($) {
+    'use strict';
+
+    /*********************************************************************************/
+    /*****   COMPONENT CONSTRUCTOR                                       ************/
+    /*******************************************************************************/
+
+    window.app.rightPanel = function() {
+
+        var model,
+
+            /***** PRIVATE FUNCTIONS ************/
+
+
+            // checks the height for component (columnHeight) to match height to right panel
+            fixHeight = function() {
+
+                if ($(window).height() > 767) {
+                    $(model.columnHeight).css('height', $(model.columnHeight).height(););
+                } else {
+                    $(model.container).removeAttr('style');
+                }
+
+            },
+
+            /***** PUBLIC FUNCTION/INITIALISE ************/
+
+            init = function(args) {
+                args = args || {};
+                model = {
+                    init: function() {
+                        this.container = args.container || '.right-panel';
+                        this.columnHeight = args.columnHeight || '.columnHeight';
+
+
+                    }
+                };
+
+                // On document ready
+                model.init();
+
+                fixHeight();
+                $(window).resize(function(event) {
+                    /* Act on the event */
+                    fixHeight();
+                });
+
+
+            },
+
+            reset = function() {
+
+            };
+
+        // Return public functions only
+        return {
+            init: init,
+            reset: reset
+        };
+    };
+
+
+
+    /*********************************************************************************/
+    /*****   ON DOCUMENT LOAD                                            ************/
+    /*******************************************************************************/
+
+    $(function() {
+        if ($('.right-panel').length) {
+            var newRightPanel = new app.rightPanel();
+            newRightPanel.init({
+                container: '.right-panel'
+            });
+        }
+    });
+
+})(jQuery);
